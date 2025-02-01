9c6c28d031df0d93d57b9747f6bdc40820370f04
BALA KUMARAN M <BALAKUMARANCSE2022@GMAIL.COM>
2025-02-01 17:10:10 +0530

    Update arithmetic.sol

--- arithmetic.sol
+++ arithmetic.sol
@@ -6,10 +6,7 @@ pragma solidity >=0.8.2 <0.9.0;
 
 contract Arithmetic { 
 	
-function arithop(uint a, uint b) public pure returns (uint, uint, 
-													uint, uint, 
-													uint, uint, 
-													uint) { 
+function arithop(uint a, uint b) public pure returns (uint, uint, uint, uint, uint, uint, uint) { 
 
                                                             uint sum = a + b; 
 	
